f2bc37ae48240a6ca9fac2eda1d2fb6e127f53b5
jwaloen <jwaloen.dev@gmail.com>
2026-04-11 20:44:27 +0200

    Remove risk of dangling pointers.

--- src/MQTTClientCpp.cpp
+++ src/MQTTClientCpp.cpp
@@ -23,9 +23,6 @@ namespace mqtt{
         struct Client::Impl{
                 Impl(std::string_view uri, std::string_view clientId) : m_uri(uri), m_clientId(clientId) {}
                 std::unique_ptr<void, MqttClientDestructor> client = nullptr;
-                MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-                MQTTClient_message pubmsg = MQTTClient_message_initializer;
-                MQTTClient_deliveryToken token;
                 std::string m_uri;
                 std::string m_clientId;
                 bool m_connected = false;
@@ -66,12 +63,14 @@ namespace mqtt{
 
         void Client::connect(){
                 int rc = 0;
-                pImpl->conn_opts.keepAliveInterval = 20;
-                pImpl->conn_opts.cleansession = 1;
+                MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+
+                conn_opts.keepAliveInterval = 20;
+                conn_opts.cleansession = 1;
 
                 if (pImpl->client){
                         MQTTClient rp = pImpl->client.get();
-                        if ((rc = MQTTClient_connect(rp, &pImpl->conn_opts)) != MQTTCLIENT_SUCCESS)
+                        if ((rc = MQTTClient_connect(rp, &conn_opts)) != MQTTCLIENT_SUCCESS)
                         {
                                 throw std::runtime_error("Failed to connect to MQTT Client with C library.\n");
                         }
@@ -84,20 +83,22 @@ namespace mqtt{
 
         void Client::publish(const std::string& topic, const std::string& msg){
                 int rc = 0;
+                MQTTClient_message pubmsg = MQTTClient_message_initializer;
+                MQTTClient_deliveryToken token;
 
-                pImpl->pubmsg.payload = const_cast<char*>(msg.c_str());
-                pImpl->pubmsg.payloadlen = msg.length();
-                pImpl->pubmsg.qos = 1;
-                pImpl->pubmsg.retained = 0;
+                pubmsg.payload = const_cast<char*>(msg.c_str());
+                pubmsg.payloadlen = msg.length();
+                pubmsg.qos = 1;
+                pubmsg.retained = 0;
 
                 if (pImpl->m_connected){
                         MQTTClient rp = pImpl->client.get();
-                        if ((rc = MQTTClient_publishMessage(rp, topic.c_str(), &pImpl->pubmsg, &pImpl->token)) != MQTTCLIENT_SUCCESS)
+                        if ((rc = MQTTClient_publishMessage(rp, topic.c_str(), &pubmsg, &token)) != MQTTCLIENT_SUCCESS)
                         {
                                 throw std::runtime_error("Failed to publish message, return code %d\n");
                         }
 
-                        rc = MQTTClient_waitForCompletion(rp, pImpl->token, 10000L);
+                        rc = MQTTClient_waitForCompletion(rp, token, 10000L);
                 }
                 else {
                         throw std::runtime_error("Cannot publish before we are connected to a client\n");
