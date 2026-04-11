eb3e03983916f3d434444c96d86cebcad194feaa
jwaloen <jwaloen.dev@gmail.com>
2026-04-11 21:14:11 +0200

    members naming convention

--- src/MQTTClientCpp.cpp
+++ src/MQTTClientCpp.cpp
@@ -22,12 +22,12 @@ namespace mqtt{
 
         struct Client::Impl{
                 Impl(std::string_view uri, std::string_view clientId) : m_uri(uri), m_clientId(clientId) {}
-                std::unique_ptr<void, MqttClientDestructor> client = nullptr;
+                std::unique_ptr<void, MqttClientDestructor> m_client = nullptr;
                 std::string m_uri;
                 std::string m_clientId;
                 bool m_connected = false;
 
-                std::function<void(std::string, std::string)> msgHandler;
+                std::function<void(std::string, std::string)> m_msgHandler;
 
                 static int m_msgarrvd_trampoline(void* context, char* topicName, int topicLen, MQTTClient_message* message){
                         auto* l_impl = static_cast<Impl*>(context);
@@ -37,8 +37,8 @@ namespace mqtt{
                         std::string topic(topicName, realTopicLen);
                         std::string payload(static_cast<char*>(message->payload), message->payloadlen);
 
-                        if(l_impl->msgHandler){
-                                l_impl->msgHandler(std::move(topic), std::move(payload));
+                        if(l_impl->m_msgHandler){
+                                l_impl->m_msgHandler(std::move(topic), std::move(payload));
                         }
 
                         MQTTClient_freeMessage(&message);
@@ -53,8 +53,8 @@ namespace mqtt{
                 if ((rc = MQTTClient_create(&rp, pImpl->m_uri.c_str(), pImpl->m_clientId.c_str(), MQTTCLIENT_PERSISTENCE_NONE, nullptr)) != MQTTCLIENT_SUCCESS){
                         throw std::runtime_error("Failed to create MQTT Client with C library.\n");
                 }
-                pImpl->client.reset(rp);
-                if (pImpl->client == nullptr) {
+                pImpl->m_client.reset(rp);
+                if (pImpl->m_client == nullptr) {
                         throw std::runtime_error("Failed to store MQTT Client.\n");
                 }
         }
@@ -68,8 +68,8 @@ namespace mqtt{
                 conn_opts.keepAliveInterval = 20;
                 conn_opts.cleansession = 1;
 
-                if (pImpl->client){
-                        MQTTClient rp = pImpl->client.get();
+                if (pImpl->m_client){
+                        MQTTClient rp = pImpl->m_client.get();
                         if ((rc = MQTTClient_connect(rp, &conn_opts)) != MQTTCLIENT_SUCCESS)
                         {
                                 throw std::runtime_error("Failed to connect to MQTT Client with C library.\n");
@@ -92,7 +92,7 @@ namespace mqtt{
                 pubmsg.retained = 0;
 
                 if (pImpl->m_connected){
-                        MQTTClient rp = pImpl->client.get();
+                        MQTTClient rp = pImpl->m_client.get();
                         if ((rc = MQTTClient_publishMessage(rp, topic.c_str(), &pubmsg, &token)) != MQTTCLIENT_SUCCESS)
                         {
                                 throw std::runtime_error("Failed to publish message, return code %d\n");
@@ -107,8 +107,8 @@ namespace mqtt{
 
         void Client::setCallbacks(std::function<void(std::string, std::string)> msgarrvd){
                 int rc = 0;
-                MQTTClient rp = pImpl->client.get();
-                pImpl->msgHandler = msgarrvd;
+                MQTTClient rp = pImpl->m_client.get();
+                pImpl->m_msgHandler = msgarrvd;
 
                 
                 if ((rc = MQTTClient_setCallbacks(rp, pImpl.get(), nullptr, pImpl->m_msgarrvd_trampoline, nullptr)) != MQTTCLIENT_SUCCESS){
@@ -119,7 +119,7 @@ namespace mqtt{
 
         void Client::subscribe(const std::string& topic){
                 int rc = 0;
-                MQTTClient rp = pImpl->client.get();
+                MQTTClient rp = pImpl->m_client.get();
 
                 if (pImpl->m_connected){
                         if ((rc = MQTTClient_subscribe(rp, topic.c_str(), 1)) != MQTTCLIENT_SUCCESS){
@@ -135,7 +135,7 @@ namespace mqtt{
         void Client::disconnect(){
                 int rc = 0;
                 if (pImpl->m_connected){
-                        MQTTClient rp = pImpl->client.get();
+                        MQTTClient rp = pImpl->m_client.get();
 
                         if ((rc = MQTTClient_disconnect(rp, 10000)) != MQTTCLIENT_SUCCESS){
                                 throw std::runtime_error("Failed to disconnect, return code %d\n");
